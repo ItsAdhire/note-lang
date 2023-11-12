3d861d26acafd6c919872187939fc1b76720c762
Nishil Patel <nishil.patel13@gmail.com>
2023-11-12 13:25:04 -0500

    implemented subdivisions

--- src/note_lang/audio.clj
+++ src/note_lang/audio.clj
@@ -1,41 +1,11 @@
 (ns note-lang.audio
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [note-lang.wave :as wave]))
 
 (import javax.sound.sampled.AudioInputStream
         javax.sound.sampled.AudioFormat
         javax.sound.sampled.AudioFileFormat$Type
         javax.sound.sampled.AudioSystem)
-
-(defn- sin [x] 
- (Math/sin x))
-
-(defn wave [freq vol bitrate duration]
-  (let [domain  (* freq 2 Math/PI)
-        amp     (partial * vol)
-        step    (/ domain bitrate)
-        samples (* bitrate duration)]
-    (->> (range)
-         (map (comp amp sin (partial * step)))
-         (take samples))))
-
-;-------
-
-(def note->freq
-  {:e4 329.63
-   :a4 440
-   :c5 523.25
-   :e5 659.25
-   :g5 783.99
-   :b5 987.77
-   :rest 0})
-
-
-(defn song->wave [song vol bitrate duration]
- (->> song
-      (map note->freq)
-      (mapcat #(wave % vol bitrate duration))))
-
-;-------
 
 (defn- create-format [bitrate]
   (new AudioFormat bitrate 8 1 true true))
@@ -73,19 +43,19 @@
 
 ;-------
 
-(defn- apply-song-as-wave [song vol bitrate duration f]
-      (f (song->wave song vol bitrate duration)))
+(defn- apply-song-as-wave [song vol bitrate tempo f]
+      (f (wave/song->wave song vol bitrate tempo)))
 
-(defn save [song vol bitrate duration file-path]
+(defn save [song vol bitrate tempo file-path]
   (apply-song-as-wave song
                       vol 
                       bitrate
-                      duration 
+                      tempo 
                       #(save-wave % bitrate file-path)))
 
-(defn play [song vol bitrate duration]
+(defn play [song vol bitrate tempo]
   (apply-song-as-wave song
                       vol 
                       bitrate
-                      duration 
+                      tempo
                       #(play-wave % bitrate)))
--- src/note_lang/core.clj
+++ src/note_lang/core.clj
@@ -1,14 +1,25 @@
 (ns note-lang.core
   (:require [note-lang.audio :as audio]))
 
-(def song (take 120 (cycle '(:a4 :c5 :e5 :c5 :e5 :c5 :a4 :c5))))
-(def song2 (take 120 (cycle '(:e5 :g5 :b5 :g5 :b5 :g5 :e5 :g5))))
-(def song3 (take 240 (cycle (concat 
-                              (repeat 8 :rest)
-                              '(:e4 :e5 :e4 :e5)
-                              (repeat 4 :rest)))))
+(def note->freq
+  {:e4 329.63
+   :a4 440
+   :c5 523.25
+   :e5 659.25
+   :g5 783.99
+   :b5 987.77
+   :a5 880
+   :rest 0})
 
-(def time-scale 0.5)
-(def bitrate 8800)
-(def volume 4)
+(def song 
+  [[:e4 :a4 :c5 :e5]
+   [:e4 :e5 :c5 :rest]
+   (take 4 (repeat :rest))
+   [:b5 :a5 :g5 :c5]])
 
+(def song2
+  [:e4 :a4 :c5 :e5
+   [:e4 :e5] [:c5 :rest] [:b5 :rest] [:g5 :c5]
+   :b5 :rest :g5 :c5]) 
+
+(audio/play song2 30 4400 210)
